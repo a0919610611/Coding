645e84da98859ad67ab98f2e4e1b356bcba908ca
傅裕夫 <a0919610611@gmail.com>
2016-09-07 16:07:33 +0800

    Update

--- Graph/Test/1330_Targan.cpp
+++ Graph/Test/1330_Targan.cpp
@@ -1,7 +1,15 @@
-#include <bits/stdc++.h>
+/*
+ test for LCA Targan
+ */
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <utility>
+#include <vector>
+
 using namespace std;
 //Tarjan
-const int MAXN = 1000; //max_number of nodes
+const int MAXN = 10000 + 5; //max_number of nodes
 int n;
 int root;
 int indeg[MAXN]; //入度
@@ -9,7 +17,7 @@ vector<int> tree[MAXN];
 void inputTree()
 {
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
         tree[i].clear(), indeg[i] = 0;
     for (int i = 0; i < n - 1; i++) {
         int x, y;
@@ -17,31 +25,35 @@ void inputTree()
         tree[x].push_back(y);
         indeg[y]++;
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
         if (indeg[i] == 0) {
             root = i;
             break;
         }
 }
 vector<int> query[MAXN];
+vector<pair<int, int> > Q;
 void inputQuery()
 {
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         query[i].clear();
     }
+    Q.clear();
     int m; //number of query
-    cin >> m;
+    //cin >> m;
+    m = 1;
     while (m--) {
         int u, v;
         cin >> u >> v;
         query[u].push_back(v);
         query[v].push_back(u);
+        Q.push_back(make_pair(u, v));
     }
 }
 int father[MAXN], depth[MAXN];
 void makeSet()
 {
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         father[i] = i, depth[i] = 0;
     }
 }
@@ -65,33 +77,40 @@ void unionSet(int x, int y)
     }
 }
 int ancestor[MAXN];
-int lca[MAXN][MAXN];
+//int lca[MAXN][MAXN];
 bool visited[MAXN];
 void Tarjan(int u)
 {
-    for(int i=0;i<tree[u].size();i++)
-    {
-        int v=tree[u][i];
+    for (int i = 0; i < tree[u].size(); i++) {
+        int v = tree[u][i];
         Tarjan(v);
-        unionSet(u,v);
-        ancestor[findSet(u)]=u;
+        unionSet(u, v);
+        ancestor[findSet(u)] = u;
     }
-    visited[u]=true;
-    for(int i=0;i<query[u].size();i++)
-    {
-        int v=query[u][i];
-        if(visited[v])
-        {
-            lca[u][v]=lca[u][v]=ancestor[findSet(v)];
+    visited[u] = true;
+    for (int i = 0; i < query[u].size(); i++) {
+        int v = query[u][i];
+        if (visited[v]) {
+            //lca[u][v] = lca[u][v] = ancestor[findSet(v)];
+            cout << ancestor[findSet(v)]<<endl;;
+            return;
         }
     }
 }
-//double algorithm 倍增算法
-int height;
-void dfs(int u)
+int main()
 {
-    for(int i=0;i<tree[x].size();i++)
-    {
-        int v=tree[x][i];
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while (T--) {
+        inputTree();
+        inputQuery();
+        makeSet();
+        memset(ancestor, 0, sizeof(ancestor));
+        memset(visited, 0, sizeof(visited));
+        Tarjan(root);
     }
+
+    return 0;
 }
